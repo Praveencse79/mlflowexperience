d56bf72e8712b49b39944b6297bc91b892b511ec
Praveencse79 <praveenhelp79@gmail.com>
2025-04-06 12:08:02 +0530

    adding the .gitignore files

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # mlflowexperience
 
 ## How to run this project 
+# First cmd conda create -p venv python==3.9 -y
+# 2nd command pip install -r requirements.txt (To install all packages mention in the requirements.txt file)
+# Create a file requirements.txt and app.py
+# and run the app.py files
+# run mlflow ui
+# Problem	Solution
+# 500 Errors	Likely DagsHub server issue – retry later
+# 403 Forbidden	Check your API token / login credentials
+# Can't log model	Avoid reusing stale or failed run IDs
+# DagsHub Down	Temporarily switch to local MLflow or wait/retry
--- app.py
+++ app.py
@@ -75,8 +75,8 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        #predictions = lr.predict(train_x)
-        #signature = infer_signature(train_x, predictions)
+        predictions = lr.predict(train_x)
+        signature = infer_signature(train_x, predictions)
 
         ## For Remote server only(DAGShub)
 
@@ -90,9 +90,10 @@ if __name__ == "__main__":
             # Register the model
             # There are other ways to use the Model Registry, which depends on the use case,
             # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            # https://mlflow.org/docs/latest/model-registry.html api-workflow
             mlflow.sklearn.log_model(
-                lr, "model", registered_model_name="ElasticnetWineModel"
+                lr, "model", registered_model_name="ElasticnetWineModel" ,
             )
         else:
-            mlflow.sklearn.log_model(lr, "model")
+            mlflow.sklearn.log_model(lr, "model",)
+ 
